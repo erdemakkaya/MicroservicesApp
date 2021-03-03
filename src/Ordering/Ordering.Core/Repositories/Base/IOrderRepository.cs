2edb934dd1b6cb354eef9e1e1dab3b0e344dc597
erdem <erdem@abisteknoloji.com.tr>
2021-03-03 22:57:13 +0300

    Repository Interfaces added

---
+++ src/Ordering/Ordering.Core/Repositories/Base/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Ordering.Core.Entities;
+
+namespace Ordering.Core.Repositories.Base
+{
+   public interface IOrderRepository:IRepository<Order>
+   {
+       Task<IEnumerable<Order>> GetOrderByUserName(string userName);
+   }
+}
