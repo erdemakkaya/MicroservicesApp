246dafc81b1e36ed0e91eaa1a94a7bb8f35d44f3
erdem akkaya <erdem_akkaya1@outlook.com>
2021-03-17 22:35:33 +0300

    Web App Added to Solution

---
+++ src/WebApp/AspnetRunBasics/ApiCollection/Interfaces/ICatalog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ApiCollection.Interfaces
+{
+    public interface ICatalogApi
+    {
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+        Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+        Task<CatalogModel> GetCatalog(string id);
+        Task<CatalogModel> CreateCatalog(CatalogModel model);
+    }
+}
